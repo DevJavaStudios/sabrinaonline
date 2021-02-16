513e8774c95201be04346fd39c6b28ca03f471d4
DevJavaStudios <77597969+DevJavaStudios@users.noreply.github.com>
2021-02-15 22:47:58 -0300

    Create suporte.java

---
+++ Android/suporte.java
@@ -0,0 +1,9 @@
+/*
+ * Classname             (sabrinabot)
+ *
+ * Version information   (3.0)
+ *
+ * Date                  (06/12,15:35)
+ *
+ * author                (Dev Java)
+ * Copyright notice      (bot com suporte a Android/Java)
