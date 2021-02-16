405cd03587b6fb6e4b06277da6400db8577ffe90
DevJavaStudios <77597969+DevJavaStudios@users.noreply.github.com>
2021-02-15 22:49:41 -0300

    Create suporte.java

---
+++ Komputer/suporte.java
@@ -0,0 +1,5 @@
+Pc = komputer * (darkbot3.0 + sabrinabot - dark)
+           + 4 * bot; // PREFER
+
+Android = /0/android/suporte.java * (darkbot3.0 + dark
+                       - sabrinabot) + 4 * bot; // AVOID
